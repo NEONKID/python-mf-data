208f5a00729995aeec840849ea617959ce678865
NEONKID <contact@neonkid.xyz>
2022-03-14 22:31:38 +0900

    Init AsyncRepository, SyncRepository..

--- pymfdata/mongodb/repository.py
+++ pymfdata/mongodb/repository.py
@@ -1,42 +1,86 @@
-from abc import ABC
 from bson import ObjectId
-from typing import final, List, Optional
+from typing import final, List, Optional, Protocol
+from motor.core import AgnosticBaseCursor, AgnosticCollection, Cursor
+from pymongo.collection import Collection
 
-from pymfdata.mongodb.connection import AsyncMotor
+from pymfdata.mongodb.connection import AsyncPyMongo, SyncPyMongo
+from pymfdata.mongodb.models import _T, _MT
 
 
-class AsyncRepository(ABC):
-    def __init__(self, collection_name: str, motor: AsyncMotor) -> None:
-        self._collection = motor.client[motor.db_name][collection_name]
+class AsyncRepository(Protocol[_MT, _T]):
+    _collection: AgnosticCollection
+
+    @property
+    def collection(self) -> AgnosticCollection:
+        assert self._collection is not None
+        return self._collection
+
+    # def __init__(self, collection_name: str, motor: AsyncPyMongo) -> None:
+    #     self._collection: AgnosticCollection = motor.client[motor.db][collection_name]
 
     @final
-    async def delete_by_id(self, item_id: str) -> bool:
-        row = await self._collection.delete_one({"_id": ObjectId(item_id)})
+    async def delete_by_id(self, item_id: _T) -> bool:
+        row = await self.collection.delete_one({"_id": item_id})
         if not row:
             return False
 
         return True
 
     @final
-    async def find_all(self) -> List[dict]:
-        cursor = self._collection.find()
-        results = list(map(lambda item: item, await cursor.to_list(length=100)))
-
-        return results
+    async def find_all(self, **kwargs) -> List[dict]:
+        cursor: AgnosticBaseCursor = self.collection.find()
+        return list(map(lambda item: item, await cursor.to_list(**kwargs)))
 
     @final
-    async def find_by_id(self, item_id: str) -> Optional[dict]:
-        row = await self._collection.find_one({"_id": ObjectId(item_id)})
+    async def find_by_id(self, item_id: _T) -> Optional[dict]:
+        row = await self.collection.find_one({"_id": item_id})
         if not row:
             return None
 
         return row
 
     @final
-    async def save(self, req: dict) -> dict:
-        return await self._collection.insert_one(req)
+    async def save(self, req: _MT) -> dict:
+        return await self.collection.insert_one(req.dict())
 
     @final
-    async def update_by_id(self, item_id: str, req: dict) -> dict:
-        await self._collection.update_one({"_id": ObjectId(item_id)}, req)
-        return await self.find_by_id(item_id)
+    async def update_by_id(self, item_id: _T, req: _MT):
+        await self.collection.update_one({"_id": item_id}, req.dict())
+
+
+class SyncRepository(Protocol[_MT, _T]):
+    _collection: Collection
+
+    @property
+    def collection(self) -> Collection:
+        assert self._collection is not None
+        return self._collection
+
+    @final
+    def delete_by_id(self, item_id: _T) -> bool:
+        row = self.collection.find_one({"_id": item_id})
+        if not row:
+            return False
+
+        return True
+
+    @final
+    def find_all(self, **kwargs) -> List[dict]:
+        cursor: Cursor = self.collection.find(**kwargs)
+        return list(map(lambda item: item, cursor))
+
+    @final
+    def find_by_id(self, item_id: _T) -> Optional[dict]:
+        row = self.collection.find_one({"_id": item_id})
+        if not row:
+            return None
+
+        return row
+
+    @final
+    def save(self, req: _MT) -> _T:
+        return self.collection.insert_one(req.dict()).inserted_id
+
+    @final
+    def update_by_id(self, item_id: _T, req: _MT):
+        self.collection.update_one({"_id": item_id}, req.dict())
